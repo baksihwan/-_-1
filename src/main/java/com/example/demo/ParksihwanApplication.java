e9df0812668d1c61b76ea2b06ebe97dd126c31e4
baksihwan <baksh999@naver.com>
2024-05-02 21:28:08 +0900

    스프링부트 첫 세팅

---
+++ src/main/java/com/example/demo/ParksihwanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksihwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParksihwanApplication.class, args);
+	}
+
+}
