018765674a316ccbf452a5cf91340be41ea1ac6a
baksihwan <baksh999@naver.com>
2024-05-26 20:58:58 +0900

    test 서버 엔티티 기능 구현연습

--- src/test/java/com/example/demo/ParksihwanApplicationTests.java
+++ src/test/java/com/example/demo/ParksihwanApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ParksihwanApplicationTests {
 
@@ -14,19 +16,9 @@ class ParksihwanApplicationTests {
 	private QuestionRepository questionRepository;
 
 
-
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		Question q = this. questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1,q.getId());
 	}
 }
