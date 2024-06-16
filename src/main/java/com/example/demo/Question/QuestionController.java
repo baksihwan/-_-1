922572b131abca3c62a001319109c4a55ba98461
baksihwan <baksh999@naver.com>
2024-06-16 13:21:13 +0900

    URL 프리픽스 설정코드

--- src/main/java/com/example/demo/Question/QuestionController.java
+++ src/main/java/com/example/demo/Question/QuestionController.java
@@ -11,20 +11,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
-
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String question(Model model) {
         List<Question> questionlist = this.questionService.list();
         model.addAttribute("questionlist", questionlist);
         return "question_list";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
