8bab49d42c697f8fa415f5ec745d8719df18d4cb
baksihwan <baksh999@naver.com>
2024-06-16 13:15:00 +0900

    상세 페이지 만드는 코드법

--- src/test/java/com/example/demo/ParksihwanApplicationTests.java
+++ src/test/java/com/example/demo/ParksihwanApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -17,7 +19,16 @@ class ParksihwanApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q = this. questionRepository.findBySubject("sbb가 무엇인가요?");
-		assertEquals(1,q.getId());
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb란 단어는 저 또한 모르겠습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("id는 자동으로 생성되나요?");
+		q2.setContent("스프링부트 모델 질문입니다.");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
 	}
 }
