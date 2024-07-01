7ef151ddd43a985be634a7193c799e339c825567
baksihwan <baksh999@naver.com>
2024-07-01 11:19:48 +0900

    css를 적용해서 웹페이지 디자인

--- src/main/java/com/example/demo/Answer/AnswerController.java
+++ src/main/java/com/example/demo/Answer/AnswerController.java
@@ -13,13 +13,12 @@ import org.springframework.web.bind.annotation.*;
 public class AnswerController {
     private final QuestionService questionService;
     private final AnswerService answerService;
-
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id")Integer id
-    , @RequestParam(value="content") String content){
-        Question question = this. questionService.getQuestion(id);
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content){
+        Question question = this.questionService.getQuestion(id);
         this.answerService.create(question,content);
-        return String.format("redirect:/question/detail/$s",id);
+
+        return String.format("redirect:/question/detail/%s", id);}
     }
 
-}
+
--- src/main/java/com/example/demo/Answer/AnswerService.java
+++ src/main/java/com/example/demo/Answer/AnswerService.java
@@ -11,7 +11,7 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
 
     public void create(Question question, String content){
-        Answer answer = new Answer();
+        Answer answer  = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
